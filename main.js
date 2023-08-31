9ea25c2d88d0915bd90cc1793a4c038184f7f9e0
ArthurSpada <113390584+ArthurSpada@users.noreply.github.com>
2023-08-31 18:57:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-narizx=0;
-narizy=0
+olhoesquerdox=0;
+olhoesquerdoy=0;
+olhodireitox=0;
+olhodireitoy=0;
 
 function preload(){
-    bigodaodao= loadImage('Bigodaodao.png');
+    starent= loadImage('starent.png');
 }
 function setup()
 {
@@ -21,17 +23,22 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        narizx=0;
-        narizy=0;
-        console.log("nariz x =" + results[0].pode.nose.x);
-        console.log("nariz y =" + results[0].pode.nose.y);
+        olhoesquerdox=results[0].pose.leftEye.x;
+        olhoesquerdoy=results[0].pose.leftEye.y;
+        olhodireitox=results[0].pose.rightEye.x;
+        olhodireitoy=results[0].pose.rightEye.y;
+        console.log("olhoesquerdox = " + results[0].pose.leftEye.x);
+        console.log("olhoesquerdoy = " + results[0].pose.leftEye.y);
+        console.log("olhodireitox = " + results[0].pose.rightEye.x);
+        console.log("olhodireitoy = " + results[0].pose.rightEye.y);
     }
 }
 function draw(){
 image(video,0,0,300,300);
-
+image(starent,olhoesquerdox-20,olhoesquerdoy-20,40,40);
+image(starent,olhodireitox-20,olhodireitoy-20,40,40);
 }
-function take_snapshot(){
+function takeSnapshot(){
     save('myFilterImage.png');
 }
 function setup(){
