7f71d6a179a935d84abef4d7029a7e1810a0b7d1
ArthurSpada <113390584+ArthurSpada@users.noreply.github.com>
2023-09-05 19:31:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-olhoesquerdox=0;
-olhoesquerdoy=0;
-olhodireitox=0;
-olhodireitoy=0;
+narizx=0;
+narizy=0
 
 function preload(){
-    starent= loadImage('starent.png');
+    bigodaodao= loadImage('Bigodaodao.png');
 }
 function setup()
 {
@@ -23,20 +21,15 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        olhoesquerdox=results[0].pose.leftEye.x;
-        olhoesquerdoy=results[0].pose.leftEye.y;
-        olhodireitox=results[0].pose.rightEye.x;
-        olhodireitoy=results[0].pose.rightEye.y;
-        console.log("olhoesquerdox = " + results[0].pose.leftEye.x);
-        console.log("olhoesquerdoy = " + results[0].pose.leftEye.y);
-        console.log("olhodireitox = " + results[0].pose.rightEye.x);
-        console.log("olhodireitoy = " + results[0].pose.rightEye.y);
+        narizx=results[0].pose.nose.x;
+        narizy=results[0].pose.nose.y;
+        console.log("nariz x =" + results[0].pose.nose.x);
+        console.log("nariz y =" + results[0].pose.nose.y);
     }
 }
 function draw(){
 image(video,0,0,300,300);
-image(starent,olhoesquerdox-20,olhoesquerdoy-20,40,40);
-image(starent,olhodireitox-20,olhodireitoy-20,40,40);
+image(bigodaodao,narizx-27.5,narizy-5,60,60);
 }
 function takeSnapshot(){
     save('myFilterImage.png');
